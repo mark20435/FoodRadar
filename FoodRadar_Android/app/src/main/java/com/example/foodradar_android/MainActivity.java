af491b5d3f397231b1c31c06aaf1b778d23e0a78
xuan <>
2020-09-02 19:28:52 +0800

    1.公版頁初版(底部BottonBar及頂部AppBar)

--- FoodRadar_Android/app/src/main/java/com/example/foodradar_android/MainActivity.java
+++ FoodRadar_Android/app/src/main/java/com/example/foodradar_android/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.foodradar_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater(); //inflater載入器
+        inflater.inflate(R.menu.appbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        return super.onContextItemSelected(item);
     }
 }
