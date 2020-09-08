0f59045a8a4bd35e8c3b29acb51cbc83da9b561e
Mark <mark20435@hotmail.com>
2020-09-08 16:32:33 +0800

    更新餐廳資訊維護 並加上拍照相關程式

--- FoodRadar_Android/app/src/main/java/com/example/foodradar_android/user/ResInsertFragment.java
+++ FoodRadar_Android/app/src/main/java/com/example/foodradar_android/user/ResInsertFragment.java
@@ -1,24 +1,58 @@
 package com.example.foodradar_android.user;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 import com.example.foodradar_android.Common;
 import com.example.foodradar_android.R;
+import com.yalantis.ucrop.UCrop;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ResInsertFragment extends Fragment {
-    Activity activity;
     private NavController navController;
+    private final static String TAG = "TAG_ResInsertFragment";
+    private FragmentActivity activity;
+    private ImageView ivRes;
+    private EditText etResName, etResAddress, etResTel, etResHours;
+    private Spinner spCategory;
+    private Switch swResEnable;
+    private byte[] image;
+    private static final int REQ_TAKE_PICTURE = 0;
+    private static final int REQ_PICK_PICTURE = 1;
+    private static final int REQ_CROP_PICTURE = 2;
+    private Uri contentUri;
 
 
     @Override
@@ -57,5 +91,104 @@ public class ResInsertFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         activity.setTitle(R.string.titleResInsert);
         return inflater.inflate(R.layout.fragment_res_insert, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ivRes = view.findViewById(R.id.ivRes);
+        etResName = view.findViewById(R.id.etResName);
+        etResAddress = view.findViewById(R.id.etResAddress);
+        etResTel = view.findViewById(R.id.etResTel);
+        etResHours = view.findViewById(R.id.etResHours);
+        spCategory = view.findViewById(R.id.spCategory);
+        swResEnable = view.findViewById(R.id.swResEnable);
+
+        Button btTakePicture = view.findViewById(R.id.btTakePicture);
+        btTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // 指定存檔路徑
+                File file = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                file = new File(file, "picture.jpg");
+                contentUri = FileProvider.getUriForFile(
+                        activity, activity.getPackageName() + ".provider", file);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
+
+                if (intent.resolveActivity(activity.getPackageManager()) != null) {
+                    startActivityForResult(intent, REQ_TAKE_PICTURE);
+                } else {
+                    Common.showToast(activity, R.string.textNoCameraApp);
+                }
+            }
+        });
+
+        Button btPickPicture = view.findViewById(R.id.btPickPicture);
+        btPickPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, REQ_PICK_PICTURE);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case REQ_TAKE_PICTURE:
+                    crop(contentUri);
+                    break;
+                case REQ_PICK_PICTURE:
+                    crop(intent.getData());
+                    break;
+                case REQ_CROP_PICTURE:
+                    handleCropResult(intent);
+                    break;
+            }
+        }
+    }
+
+    private void crop(Uri sourceImageUri) {
+        File file = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        file = new File(file, "picture_cropped.jpg");
+        Uri destinationUri = Uri.fromFile(file);
+        UCrop.of(sourceImageUri, destinationUri)
+//                .withAspectRatio(16, 9) // 設定裁減比例
+//                .withMaxResultSize(500, 500) // 設定結果尺寸不可超過指定寬高
+                .start(activity, this, REQ_CROP_PICTURE);
+    }
+
+    private void handleCropResult(Intent intent) {
+        Uri resultUri = UCrop.getOutput(intent);
+        if (resultUri == null) {
+            return;
+        }
+        Bitmap bitmap = null;
+        try {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+                bitmap = BitmapFactory.decodeStream(
+                        activity.getContentResolver().openInputStream(resultUri));
+            } else {
+                ImageDecoder.Source source =
+                        ImageDecoder.createSource(activity.getContentResolver(), resultUri);
+                bitmap = ImageDecoder.decodeBitmap(source);
+            }
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            image = out.toByteArray();
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        }
+        if (bitmap != null) {
+            ivRes.setImageBitmap(bitmap);
+        } else {
+            ivRes.setImageResource(R.drawable.no_image);
+        }
     }
 }
