b6f1a61e847862e0603c27bcbc0578ace17d5e86
Mark <mark20435@hotmail.com>
2020-09-08 09:14:55 +0800

    修改餐廳資訊維護相關頁面

--- FoodRadar_Android/app/src/main/java/com/example/foodradar_android/user/ResMaintainFragment.java
+++ FoodRadar_Android/app/src/main/java/com/example/foodradar_android/user/ResMaintainFragment.java
@@ -16,6 +16,7 @@ import android.view.ViewGroup;
 
 import com.example.foodradar_android.Common;
 import com.example.foodradar_android.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class ResMaintainFragment extends Fragment {
@@ -57,5 +58,19 @@ public class ResMaintainFragment extends Fragment {
                              Bundle savedInstanceState) {
         activity.setTitle(R.string.resMaintain);
         return inflater.inflate(R.layout.fragment_res_maintain, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FloatingActionButton btAdd = view.findViewById(R.id.btAdd);
+        btAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view)
+                        .navigate(R.id.action_resMaintainFragment_to_resInsertFragment);
+            }
+        });
     }
 }
